0b5f3f0fe721daad73232678ac0409e8ca10f7b0
Christian Uhl <uhl.christian@googlemail.com>
2023-02-06 13:11:38 +0100

    update link to blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "Folge 0: Wo sind wir jetzt?",
+        "excerpt":"Ich wurde von zu vielen Leuten genötigt diese Geschichte zu erzählen und das als Podcast zu veröffentlichen. Also hab ich nachgegeben In Folge 0 geht es erstmal darum zu erklären warum ich das überhaupt tue, aus einer tollen Anstellung raus bei codecentric in eine ganz andere Aufgabe zu gehen. Nicht...","categories": ["podcast"],
+        "tags": [],
+        "url": "/0/",
+        "teaser": null
+      },{
+        "title": "Folge 1: Steig ein",
+        "excerpt":"Recruiting, gute Interviews führen, und warum es schwierig ist keine Technik mehr zu machen. Wir reden darüber wie ich mein Team aufgebaut habe, und was eigentlich ein gutes Interview ausmacht. Aussserdem viele gelernte Lektionen was man beim Interviewprozess falsch machen kann. Ausserdem, was macht man denn, wenn man keinen Code...","categories": ["podcast"],
+        "tags": [],
+        "url": "/1/",
+        "teaser": null
+      },{
+        "title": "Folge 2: One on Ones",
+        "excerpt":"Wir sprechen heute über One on Ones, ein Format bei dem sich, simpel gesagt, der Vorgesetzte mit dem Mitarbeiter unter vier Augen unterhalten. Ich glaube diese “One on Ones” sind unglaublich wichtig und wertvoll. Wir finden raus warum ich das glaube, und was ich aus den One on Ones mitnehme....","categories": ["podcast"],
+        "tags": [],
+        "url": "/2/",
+        "teaser": null
+      },{
+        "title": "Folge 3: Dashboards",
+        "excerpt":"Wir sprechen heute über Dashboards - Eine Möglichkeit relativ beliebige Informationen auf grossen Displays darzustellen. In fast jedem Büro hängt ein Flachbildschirm mit schönen Tortengrafiken an der Wand, aber keiner schaut hin - Warum sind mir Dashboards also so wichtig? Welche Informationen kann man mit Dashboards vermitteln, und welche Effekte...","categories": ["podcast"],
+        "tags": [],
+        "url": "/3/",
+        "teaser": null
+      },{
+        "title": "Folge 4.1: Probleme? Klar doch!",
+        "excerpt":"Update: Im ersten Versuch hatte Folge 4 einen Schnittfehler, der mit Folge 4.1 korrigiert ist! Vielen Dank an https://twitter.com/joschi83 für den Bugreport! Es läuft ja alles rosig, wenn man sich meinen Podcast so anhört, oder? Stimmt natürlich nicht, es gibt auch genug unangenehme Ereignisse, mit denen ich mich beschäftigen muss....","categories": ["podcast"],
+        "tags": [],
+        "url": "/4/",
+        "teaser": null
+      },{
+        "title": "Folge 5: One on Ones mit Benjamin Reitzammer",
+        "excerpt":"Sondersendung mit Spezialgast! Benjamin Reitzammer, Author von https://cto.coffee/, Ruby/Rails developer at https://github.com/crispymtn, ex-CTO von https://www.vaamo.de/, aktiver software crafter und noch vieles mehr. Benjamin hat schon mit seinen Blogposts für viel Input für meine One on Ones geliefert, und hat sich bereit erklärt einen double-podcast von mit mir zu exakt diesem...","categories": ["podcast"],
+        "tags": [],
+        "url": "/5/",
+        "teaser": null
+      },{
+        "title": "Folge 6: Die erste Kündigung",
+        "excerpt":"[Sorry für die schlechte Audiqualität - das Mikrofon hat aufgegeben, und wir haben gerettet was zu retten ist… die nächste Folge wird wieder besser, versprochen!] Mein erster Mitarbeiter hat mir gekündigt! Keine angenehme Erfahrung, glaubt mir. In dieser Folge arbeiten wir das auf, sprechen über Motivationen zu kündigen, und wie...","categories": ["podcast"],
+        "tags": [],
+        "url": "/6/",
+        "teaser": null
+      },{
+        "title": "Folge 7: Mob Programming Sessions",
+        "excerpt":"Heute wird es kontrovers, ich stelle eine Methode namens “Mob Programming Sessions” vor, ein Format bei dem wir bei Matmatch uns mit Mob Programming neue Technologie erschliessen und kreativ arbeiten - Ich behaupte mal ganz frech das das eins der besten Formate ist das ich kenne und eine unglaublich gute...","categories": ["podcast"],
+        "tags": [],
+        "url": "/7/",
+        "teaser": null
+      },{
+        "title": "Folge 8: Das erste Jahr (und was macht ein PO eigentlich)",
+        "excerpt":"Das erste Jahr ist vorbei! Victor und ich ziehen heute Bilanz und sprechen darüber wie es mir rückblickend ergangen ist - zumindest versuchen wir es, aber relativ schnell kommen wir zu dem Schluss dass wir ganz dringend besprechen müssen was ein Product Owner eigentlich so macht. Links: Buch: Accelerate Feedback...","categories": ["podcast"],
+        "tags": [],
+        "url": "/8/",
+        "teaser": null
+      },{
+        "title": "Cross-Funktionales Arbeiten",
+        "excerpt":"Wir arbeiten Crossfunktional, was ein tolles Buzzword! Ich bespreche mit Victor warum diese Form der Kollaboration gerade für Matmatch das absolut richtige ist, warum wir damit etwas erreichen, und welche Fallstricke uns aufgefallen sind. Es geht um KPIs, OKRs und ganz viel Moderation. Feedback gerne wieder an uhl.christian@googlemail.com oder an...","categories": ["podcast"],
+        "tags": [],
+        "url": "/9/",
+        "teaser": null
+      },{
+        "title": "Einstellen? Erst mal finden!",
+        "excerpt":"Heute mit Sondergast Jochen, Teamlead bei Holidu. Wir sprechen über Hiring: Wie findet man fähige Entwickler und Entwicklerinnen? Wie macht man gute Interviews die fair sind? Wie geht man gut mit internationalem Publikum und Visaproblemen um? Wie stellt man seine Firma gut da? Feedback gerne wieder an uhl.christian@googlemail.com oder an...","categories": ["podcast"],
+        "tags": [],
+        "url": "/10/",
+        "teaser": null
+      },{
+        "title": "Remote Arbeiten",
+        "excerpt":"Remote Arbeit ist ja wirklich in aller Munde, und jetzt müssen wir uns auch dringend mit dem Thema befassen - Ein Mitarbeiter zieht in sein Heimatland zurück, aber wir wollen ihn unbedingt halten. Also Remote! Aber wie geht das in einer Firma, die ganz und gar nicht verteilt, sondern in...","categories": ["podcast"],
+        "tags": [],
+        "url": "/11/",
+        "teaser": null
+      },{
+        "title": "Weiterbildung",
+        "excerpt":"Christian rätselt wie man Mitarbeiter denn zur Weiterbildung motiviert, ohne bestimmen zu müssen. Braucht es Weiterbildung überhaupt? Vielleicht haben wir auch schon genug gelernt? Victors innerer Coach bricht durch und übernimmt. Nebenbei besprechen wir auch ein paar lokale Konferenzen die so anstehen könnten. Feedback gerne wieder an uhl.christian@googlemail.com oder an...","categories": ["podcast"],
+        "tags": [],
+        "url": "/12/",
+        "teaser": null
+      },{
+        "title": "Junior Engineers",
+        "excerpt":"Ein freudiges Ereignis: Unser Junior Engineer hat seine Probezeit bestanden. Zeit um etwas zurückzublicken und generell über Junior Engineers zu sprechen. Warum sollte man Juniors einstellen? Was hat man davon? Und wenn ja, wann? Wie viele? Ab wann ist das Team bereit, und auf was sollte man achten? Diskutiert habe...","categories": ["podcast"],
+        "tags": [],
+        "url": "/13/",
+        "teaser": null
+      },{
+        "title": "Senior Engineers",
+        "excerpt":"Jemand aus meinem Team möchte Senior Engineer heissen - eine tolle Gelegenheit mal nachzudenken: Was macht eigentlich den Senior Engineer aus, welche Skills braucht es dafür? Ab wann kann ich jemanden zum Senior “befördern” ? Aber davor schauen wir uns mal an warum man eigentlich Senior werden will. Und am...","categories": ["podcast"],
+        "tags": [],
+        "url": "/14/",
+        "teaser": null
+      },{
+        "title": "This is the end",
+        "excerpt":"Dieser Podcast hat ein würdigeres Ende verdient als einfach nicht mehr zu erscheinen - Daher hat sich Victor aus der Frührente gequält, und wir haben einen schönen Abschluss für diese Podcast-Reihe gefunden.   Danke und macht’s gut  &lt;3  ","categories": ["podcast"],
+        "tags": [],
+        "url": "/15/",
+        "teaser": null
+      }]
